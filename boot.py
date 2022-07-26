8a3e1c929cb5f80c41edb52e474db9e090de9bc8
dobpet <95017842+dobpet@users.noreply.github.com>
2022-07-26 12:07:22 +0200

    Add files via upload

---
+++ boot.py
@@ -0,0 +1,13 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import uos, machine
+#uos.dupterm(None, 1) # disable REPL on UART(0)
+import gc
+#import webrepl
+#webrepl.start()
+gc.collect()
+
+import network
+network.WLAN(network.STA_IF).active(False)  # WiFi station interface
+network.WLAN(network.AP_IF).active(False)  # access-point interface
